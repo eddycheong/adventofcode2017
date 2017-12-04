340c11664e7efc1d63170ed1ba1e34c1a63a2962
eddycheong <cheong.e49@gmail.com>
2017-12-03 22:46:04 -0800

    Removed comment test code

--- adventofcode/inverse_captcha.py
+++ adventofcode/inverse_captcha.py
@@ -6,10 +6,3 @@ def inverse_captcha(digits):
     sum += int(digits[i]) if digits[i] == digits[(i + 1) % length] else 0
 
   return sum
-
-# Testing
-
-# print(inverse_captcha("1122"))
-# print(inverse_captcha("1111"))
-# print(inverse_captcha("1234"))
-# print(inverse_captcha("91212129"))
