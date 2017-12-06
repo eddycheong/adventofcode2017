5d73251128ff672d6ff927486112eb0c149bf260
eddycheong <cheong.e49@gmail.com>
2017-12-05 18:20:18 -0800

    Update test to include the new function names for valid_passphrase_duplicates and valid_passphrases_anagrams

--- test/test_entropy_passphrase.py
+++ test/test_entropy_passphrase.py
@@ -3,36 +3,36 @@ from adventofcode import *
 
 class TestValidPassphraseNoDuplicates(TestCase):
   def test_no_duplicates(self):
-    result = valid_passphrase("aa bb cc dd ee")
+    result = valid_passphrase_duplicates("aa bb cc dd ee")
     self.assertEqual(result, True)
 
   def test_duplicates(self):
-    result = valid_passphrase("aa bb cc dd aa")
+    result = valid_passphrase_duplicates("aa bb cc dd aa")
     self.assertEqual(result, False)
 
   def test_no_duplicates_with_subsets(self):
-    result = valid_passphrase("aa bb cc dd aaa")
+    result = valid_passphrase_duplicates("aa bb cc dd aaa")
     self.assertEqual(result, True)
 
 class TestValidPassphraseNoAnagrams(TestCase):
   def test_no_anagrams(self):
-    result = valid_passphrase("abcde fghij")
+    result = valid_passphrase_anagrams("abcde fghij")
     self.assertEqual(result, True)
 
   def test_anagrams(self):
-    result = valid_passphrase("abcde xyz ecdab")
+    result = valid_passphrase_anagrams("abcde xyz ecdab")
     self.assertEqual(result, False)
 
   def test_no_anagrams_contains_subsets(self):
-    result = valid_passphrase("a ab abc abd abf abj")
+    result = valid_passphrase_anagrams("a ab abc abd abf abj")
     self.assertEqual(result, True)
 
   def test_no_anagrams_contains_subsets(self):
-    result = valid_passphrase("iiii oiii ooii oooi oooo")
+    result = valid_passphrase_anagrams("iiii oiii ooii oooi oooo")
     self.assertEqual(result, True)
 
   def test_anagrams_permutations(self):
-    result = valid_passphrase("oiii ioii iioi iiio")
+    result = valid_passphrase_anagrams("oiii ioii iioi iiio")
     self.assertEqual(result, False)
 
 if __name__ == '__main__':
